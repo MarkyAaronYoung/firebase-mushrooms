dd3366d0e02e38c8194f4641ce7ed2aa4faf1b12
Mark Young <markyaaronyoung@gmail.com>
2020-06-20 10:47:05 -0500

    added login button that prints to dom

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,16 @@
+import auth from './components/auth/auth';
 import '../styles/main.scss';
 
 console.error('hi');
+
+//be able to login to our app
+// be able to log out of our app
+// see a login button if we're not logged in
+// seee a logout button if we ARE logged in
+// see a list of mushrooms (like a forest) if we are logged in
+
+const init = () => {
+  auth.loginButton();
+}
+
+init();
