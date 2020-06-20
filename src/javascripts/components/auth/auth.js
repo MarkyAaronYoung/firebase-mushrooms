47949fc7299043df86188ac14e27979e09b11988
Mark Young <markyaaronyoung@gmail.com>
2020-06-20 12:05:50 -0500

    added a working logout button and div hiding

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,13 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import utils from '../../helpers/utils';
 
 const signMeIn = () => {
-  console.warn('Yeah I\'ll sign you in, just give me a minute!');
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(googleProvider);
 };
 
 const loginButton = () => {
- const domString = '<button id="google-auth" class="btn btn-warning"><i class="fab fa-google-plus"></i> LOG ME IN!!!</button>'
- utils.printToDom('#auth', domString);
- $('#google-auth').click(signMeIn);
+  const domString = '<button id="google-auth" class="btn btn-warning"><i class="fab fa-google-plus"></i> LOG ME IN!!!</button>';
+  utils.printToDom('#auth', domString);
+  $('#google-auth').click(signMeIn);
 };
 
-export default { loginButton};
+export default { loginButton };
