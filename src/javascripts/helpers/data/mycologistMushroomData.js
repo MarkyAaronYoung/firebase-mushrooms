013fd1d4c812fa56d37eb78fd05696a6241a78ab
Mark Young <markyaaronyoung@gmail.com>
2020-06-23 19:20:26 -0500

    updated mycologistMyshroomData

--- src/javascripts/helpers/data/mycologistMushroomData.js
+++ src/javascripts/helpers/data/mycologistMushroomData.js
@@ -1,25 +1,22 @@
+  
 import axios from 'axios';
-import apiKeys from '../apiKeys.json'
+import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseConfig.databaseURL;
 
-
-// Get all mycologistMushrooms for a user with a specific uid
-// return as an array of objects, not an object of objects
-
 const getMycoShroomsByMycoUid = (mycoUid) => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/mycologistMushrooms.json?orderBy="mycologistUid"&equalTo="${mycoUid}"`)
-  .then((response) => {
-    const mycoShroomsObj = response.data;
-    const mycologistMushrooms = [];
-    Object.keys(mycoShroomsObj).forEach((mycoShroomId) => {
-      mycoShroomsObj[mycoShroomId].id = mycoShroomId;
-      mycologistMushrooms.push(mycoShroomsObj[mycoShroomId]);
-    });
+    .then((response) => {
+      const mycoShroomsObj = response.data;
+      const mycologistMushrooms = [];
+      Object.keys(mycoShroomsObj).forEach((mycoShroomId) => {
+        mycoShroomsObj[mycoShroomId].id = mycoShroomId;
+        mycologistMushrooms.push(mycoShroomsObj[mycoShroomId]);
+      });
 
-    resolve(mycologistMushrooms);
-  })
-  .catch((err) => reject(err));
+      resolve(mycologistMushrooms);
+    })
+    .catch((err) => reject(err));
 });
 
-export default { getMycoShroomsByMycoUid }
+export default { getMycoShroomsByMycoUid };
