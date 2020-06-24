0dbc16f62118b2d41f3c8b16fb29323e1a034c0f
Mark Young <markyaaronyoung@gmail.com>
2020-06-23 20:21:00 -0500

    added smash function, printing mushroomms that each mycologist owns to dom

--- src/javascripts/components/singleMycologist/singleMycologist.js
+++ src/javascripts/components/singleMycologist/singleMycologist.js
@@ -1,26 +1,27 @@
-import mycologistData from '../../helpers/data/mycologistData';
-import mycologistMushroomData from '../../helpers/data/mycologistMushroomData';
+import smash from '../../helpers/data/smash';
 import utils from '../../helpers/utils';
 
 import './singleMycologist.scss';
 
 const buildMycologist = (e) => {
   const mycologistId = e.target.closest('.card').id;
-  mycologistData.getMycologistById(mycologistId)
-    .then((response) => {
-      const mycologist = response.data;
-
-      // todo: take out when done with testing
-      mycologistMushroomData.getMycoShroomsByMycoUid(mycologist.uid)
-      .then((res) => console.warn('getMycoshrooms worked!', res))
-      .catch((err) => console.error('problem in getMychoShrooms', err));
-
-      const domString = `
+  smash.getSingleMycoWithShrooms(mycologistId)
+    .then((mycologist) => {
+      let domString = `
         <h2 class="text-center">Featured Mycologist</h2>
         <div class="col-12">
           <div class="card text-white bg-dark border-0 rounded-0">
             <div class="card-header">Mycologist ${mycologist.name} (Age: ${mycologist.age})</div>
-            <div class="card-body"></div>
+            <div class="card-body">
+            <h3 class="card-title">Mushroom(s) Owned:</h3>
+      `;
+
+      mycologist.mushrooms.forEach((mushroom) => {
+        domString += `<p class="card-text">${mushroom.name}</p>`;
+      });
+
+      domString += `
+            </div>
           </div>
         </div>
       `;
